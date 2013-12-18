2cdbb4a32800a5de696543eec5a90f0b2f085c1b
ccastelot <ccastelot60@gmail.com>
2013-12-18 13:28:14 -0500

    Initial commit

---
+++ Matchismo/Matchismo/csAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  csAppDelegate.h
+//  Matchismo
+//
+//  Created by Chris Castelot on 12/18/13.
+//  Copyright (c) 2013 Chris Castelot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface csAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
